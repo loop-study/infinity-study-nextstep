c698a724e3a780b0d655677d76d651a0457d0f37
apdkeb <apdkeb@naver.com>
2021-04-15 13:58:22 +0900

    test(Round) : RoundTest 생성
    2021. 04. 15.
    - RoundTest 생성
    - 라운드 관련 테스트 추가
    
    Resolves : loop-study
    See also : .

--- src/test/java/racingcar01/domain/CarTest.java
+++ src/test/java/racingcar01/domain/CarTest.java
@@ -1,6 +1,5 @@
 package racingcar01.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,7 +12,7 @@ public class CarTest {
     @ValueSource(strings = {"s","st","stu","stud","study"})
     public void car_이름_최대_5글자(String name) throws Exception {
         Car car = Car.of(name);
-        assertThat(car.name()).isEquals(name);
+        assertThat(car.name()).isEqualTo(name);
     }
 
     @Test
---
+++ src/test/java/racingcar01/domain/RoundTest.java
@@ -0,0 +1,39 @@
+package racingcar01.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RoundTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,1", "3,2", "4,3", "5,4"})
+    public void 라운드_진행(int initRound, int result) throws Exception {
+        Round round = Round.of(initRound);
+
+        Round nextRound = round.next();
+
+        assertThat(nextRound.round()).isEqualTo(result);
+    }
+
+    @Test
+    public void 라운드_종료_TRUE() throws Exception {
+        Round round = Round.of(1);
+
+        boolean result = round.next().roundIsEnd();
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void 라운드_종료_FALSE() throws Exception {
+        Round round = Round.of(2);
+
+        boolean result = round.next().roundIsEnd();
+
+        assertThat(result).isFalse();
+    }
+}
