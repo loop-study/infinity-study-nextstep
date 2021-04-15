8031f09769c760f09380e45200a80d00308d027c
apdkeb <apdkeb@naver.com>
2021-04-15 15:34:35 +0900

    test(Car, Round) : 테스트 변경
    2021. 04. 15.
    - Car, 이동 테스트 추가
    - Round, 라운드 종료 테스트 변경
    
    Resolves : loop-study
    See also : .

--- src/test/java/racingcar01/domain/CarTest.java
+++ src/test/java/racingcar01/domain/CarTest.java
@@ -2,9 +2,11 @@ package racingcar01.domain;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
@@ -19,5 +21,23 @@ public class CarTest {
     public void car_이름_5글자_초과_에러() throws Exception {
         assertThatThrownBy(() -> Car.of("123456"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void car_이동() throws Exception {
+        Car car = Car.of("loop");
+
+        int distance = car.movedCar(() -> true).distance();
+
+        assertThat(distance).isEqualTo(1);
+    }
+
+    @Test
+    public void car_이동안함() throws Exception {
+        Car car = Car.of("loop");
+
+        int distance = car.movedCar(() -> false).distance();
+
+        assertThat(distance).isEqualTo(0);
     }
 }
--- src/test/java/racingcar01/domain/RoundTest.java
+++ src/test/java/racingcar01/domain/RoundTest.java
@@ -9,17 +9,17 @@ import static org.assertj.core.api.Assertions.*;
 public class RoundTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"2,1", "3,2", "4,3", "5,4"})
-    public void 라운드_진행(int initRound, int result) throws Exception {
+    @CsvSource(value = {"1","2","3","4"})
+    public void 라운드_진행_확인(int initRound) throws Exception {
         Round round = Round.of(initRound);
 
-        Round nextRound = round.next();
-
-        assertThat(nextRound.round()).isEqualTo(result);
+        boolean result = round.next().isEnd();
+        
+        assertThat(result).isFalse();
     }
 
     @Test
-    public void 라운드_종료_TRUE() throws Exception {
+    public void 라운드_종료_확인() throws Exception {
         Round round = Round.of(1);
 
         boolean result = round.next().isEnd();
@@ -28,16 +28,7 @@ public class RoundTest {
     }
 
     @Test
-    public void 라운드_종료_FALSE() throws Exception {
-        Round round = Round.of(2);
-
-        boolean result = round.next().isEnd();
-
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void 라운드_에러확인() throws Exception {
+    public void 라운드_에러_확인() throws Exception {
         assertThatThrownBy(() -> Round.of(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
