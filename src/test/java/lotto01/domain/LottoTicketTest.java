078c7be184ff3cd1f7165cafa0542ac67abc5544
apdkeb <apdkeb@naver.com>
2021-05-13 17:46:32 +0900

    test : LottoTicketTest create
    2021. 05. 13.
    LottoTicketTest create
    
    Resolves : loop-study
    See also : .

---
+++ src/test/java/lotto01/domain/LottoTicketTest.java
@@ -0,0 +1,16 @@
+package lotto01.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("로또 티켓")
+public class LottoTicketTest {
+
+    @Test
+    public void 로또_티켓을_생성() throws Exception {
+        LottoTicket lottoTicket = LottoTicket.of();
+        assertThat(lottoTicket).isNotNull();
+    }
+}
