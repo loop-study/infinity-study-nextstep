4d0d31078640f922747881ec1078ef079ddee69e
apdkeb <apdkeb@naver.com>
2021-05-13 17:43:44 +0900

    feat : lottoNumber range valid add
    2021. 05. 13.
    로또번호 1~45 유효성 추가
    
    Resolves : loop-study
    See also : .

--- src/main/java/lotto01/domain/LottoNumber.java
+++ src/main/java/lotto01/domain/LottoNumber.java
@@ -1,6 +1,9 @@
 package lotto01.domain;
 
 public class LottoNumber {
+    private static final String OUT_RANGE_MASSAGE = "잘못된 로또 번호입니다.";
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
 
     private final int number;
 
@@ -9,10 +12,18 @@ public class LottoNumber {
     }
 
     public static LottoNumber of(int number) {
+        validateNumberRange(number);
         return new LottoNumber(number);
     }
 
     public int toInt() {
         return number;
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < MIN_RANGE
+            || number > MAX_RANGE) {
+            throw new IllegalArgumentException(OUT_RANGE_MASSAGE);
+        }
     }
 }
