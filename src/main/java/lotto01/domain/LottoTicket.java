6dd84ffc5255a72ea89a36e89fb0759f7a9cfa5f
apdkeb <apdkeb@naver.com>
2021-05-13 19:35:11 +0900

    feat : lottoTicket method add
    2021. 05. 13.
    로또 티켓 기능 추가
    - 1~45 중복안되는 랜덤번호 6개 생성
    - 자동 로또 생성
    - 수동 로또 생성
    
    Resolves : loop-study
    See also : .

--- src/main/java/lotto01/domain/LottoTicket.java
+++ src/main/java/lotto01/domain/LottoTicket.java
@@ -1,9 +1,16 @@
 package lotto01.domain;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoTicket {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final List<Integer> DEFAULT_NUMBERS = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+                                                                .boxed()
+                                                                .collect(Collectors.toList());
 
     private final List<LottoNumber> lottoNumbers;
 
@@ -12,6 +19,32 @@ public class LottoTicket {
     }
 
     public static LottoTicket of() {
-        return new LottoTicket(new ArrayList<>());
+        return new LottoTicket(createLottoNumbers(shuffleNumbers()));
+    }
+
+    public static LottoTicket of(List<Integer> manual) {
+        return new LottoTicket(createLottoNumbers(manual));
+    }
+
+    public List<LottoNumber> lottoNumbers() {
+        return lottoNumbers;
+    }
+
+    private static List<Integer> shuffleNumbers() {
+        Collections.shuffle(DEFAULT_NUMBERS);
+
+        List<Integer> numbers = DEFAULT_NUMBERS.stream()
+                                    .limit(6)
+                                    .collect(Collectors.toList());
+
+        Collections.sort(numbers);
+
+        return numbers;
+    }
+
+    private static List<LottoNumber> createLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::of)
+                .collect(Collectors.toList());
     }
 }
