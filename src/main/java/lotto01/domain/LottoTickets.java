d1c26cb8cf6cfbf5bc8d122182f0033c04ec1331
apdkeb <apdkeb@naver.com>
2021-05-14 15:09:07 +0900

    feat(LottoTickets) : create
    2021. 05. 14.
    로또 티켓 일급 객체 생성
    
    Resolves : loop-study
    See also : .

---
+++ src/main/java/lotto01/domain/LottoTickets.java
@@ -0,0 +1,16 @@
+package lotto01.domain;
+
+import java.util.List;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public List<LottoTicket> lottoTickets() {
+        return lottoTickets;
+    }
+}
