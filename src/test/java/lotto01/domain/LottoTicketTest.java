a18a594efb89705522d0e688cd811aba38599fc4
apdkeb <apdkeb@naver.com>
2021-05-14 12:02:03 +0900

    test : lottoTicket numbers size exception test add
    2021. 05. 14.
    로또 번호 6개가 아니면 에러확인 테스트 추가
    
    Resolves : loop-study
    See also : .

--- src/test/java/lotto01/domain/LottoTicketTest.java
+++ src/test/java/lotto01/domain/LottoTicketTest.java
@@ -2,11 +2,24 @@ package lotto01.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("로또 티켓")
 public class LottoTicketTest {
+
+    private List<Integer> stringToList(String inputNumbers) {
+        return Arrays.stream(inputNumbers.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+    }
 
     @Test
     public void 로또_티켓을_생성() throws Exception {
@@ -20,8 +33,10 @@ public class LottoTicketTest {
         assertThat(lottoTicket.lottoNumbers().size()).isEqualTo(6);
     }
 
-    @Test
-    public void 로또_번호가_6개가_아니면_예외처리() throws Exception {
-
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    public void 로또_번호가_6개가_아니면_예외처리(String inputNumbers) throws Exception {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoTicket.of(stringToList(inputNumbers)));
     }
 }
