256e35810a08e0afb2eae1d951225bec981a507b
apdkeb <apdkeb@naver.com>
2021-05-14 13:50:36 +0900

    feat(WinnerNumbers) : 중복 유효성 추
    2021. 05. 14.
    당첨번호 6자리와 보너스볼 중복 유효성 추가
    
    Resolves : loop-study
    See also : .

--- src/main/java/lotto01/domain/WinnerNumbers.java
+++ src/main/java/lotto01/domain/WinnerNumbers.java
@@ -1,12 +1,20 @@
 package lotto01.domain;
 
 public class WinnerNumbers {
+    private static final String DUPLICATE_MASSAGE = "보너스 번호가 중복됩니다.";
 
     private LottoTicket winnerTicket;
     private LottoNumber bonusBall;
 
     public WinnerNumbers(LottoTicket winnerTicket, LottoNumber bonusBall) {
+        validateDuplicate(winnerTicket, bonusBall);
         this.winnerTicket = winnerTicket;
         this.bonusBall = bonusBall;
+    }
+
+    private void validateDuplicate(LottoTicket winnerTicket, LottoNumber bonusBall) {
+        if (winnerTicket.lottoNumbers().contains(bonusBall)) {
+            throw new IllegalArgumentException(DUPLICATE_MASSAGE);
+        }
     }
 }
