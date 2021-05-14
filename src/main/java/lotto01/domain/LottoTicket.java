3a0a3ef7b68ab6d8718457e05e6e1c3ecde6be2c
apdkeb <apdkeb@naver.com>
2021-05-14 12:08:40 +0900

    feat : 로또 번호 6개 확인 유효성 추가
    2021. 05. 14.
    로또 번호 6개가 유효성 추가
    
    Resolves : loop-study
    See also : .

--- src/main/java/lotto01/domain/LottoTicket.java
+++ src/main/java/lotto01/domain/LottoTicket.java
@@ -1,11 +1,15 @@
 package lotto01.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoTicket {
+    private static final int DEFAULT_NUMBERS_SIZE = 6;
+    private static final String DEFAULT_NUMBERS_SIZE_MASSAGE = "로또 번호는 6개 입니다.";
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final List<Integer> DEFAULT_NUMBERS = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
@@ -23,11 +27,21 @@ public class LottoTicket {
     }
 
     public static LottoTicket of(List<Integer> manual) {
+        validateNumbersSize(manual);
         return new LottoTicket(createLottoNumbers(manual));
     }
 
     public List<LottoNumber> lottoNumbers() {
         return lottoNumbers;
+    }
+
+    private static void validateNumbersSize(List<Integer> manual) {
+        Set<Integer> validNumbers = new HashSet<>();
+        validNumbers.addAll(manual);
+
+        if (validNumbers.size() != DEFAULT_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(DEFAULT_NUMBERS_SIZE_MASSAGE);
+        }
     }
 
     private static List<Integer> shuffleNumbers() {
